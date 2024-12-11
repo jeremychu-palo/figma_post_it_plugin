81279334823e59b7ea8e64ed3c5e2322cf77e326
Jeremy Chu <jchu@palo-it.com>
2024-12-12 00:14:25 +0800

    Added a textbox and sets this text in the stickynote

--- code.js
+++ code.js
@@ -1,23 +1,32 @@
 figma.showUI(__html__);
 
-figma.ui.resize(240, 120);
+figma.ui.resize(240, 200);
 
 figma.ui.onmessage = async (msg) => {
   if (msg.type === 'create-shape') {
-    const shape = figma.createShapeWithText();
-    // Position the shape in the center of the viewport
+    console.log('Received text from UI:', msg.text);
+    
+    const sticky = figma.createSticky();
+    // Position the sticky note in the center of the viewport
     const center = figma.viewport.center;
-    shape.x = center.x;
-    shape.y = center.y;
+    sticky.x = center.x;
+    sticky.y = center.y;
     
-    // Set some basic properties
-    shape.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-    shape.text.characters = "Hello Figjam!";
+    // Set properties for the sticky note
+    sticky.fills = [{type: 'SOLID', color: {r: 1, g: 0.8, b: 0.3}}]; // Yellow color
+    // Create a text sublayer for the sticky note
+    await figma.loadFontAsync(sticky.text.fontName)
+    // textLayer.characters = msg.text;
+    // textLayer.fontSize = 16;
+    // textLayer.textAlignHorizontal = "CENTER";
+    // textLayer.textAlignVertical = "CENTER";
+    sticky.text.characters = msg.text;
+    console.log('Set sticky note text to:', msg.text);
     
-    // Select the created shape
-    figma.currentPage.selection = [shape];
+    // Select the created sticky note
+    figma.currentPage.selection = [sticky];
     
-    // Zoom into the shape
-    figma.viewport.scrollAndZoomIntoView([shape]);
+    // Zoom into the sticky note
+    figma.viewport.scrollAndZoomIntoView([sticky]);
   }
 };
